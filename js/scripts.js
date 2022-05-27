92fdae82992ffaca7d6b695452701c7fa62f00ef
appaandbuki <104958634+appaandbuki@users.noreply.github.com>
2022-05-27 15:37:04 -0700

    creating loop

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,7 @@ let pokemonList= [
   { name: 'Squirtle', height: .5, types: 'water' },
   { name: 'Pikachu', height: .4, types: 'electric' },
 ];
+//create a for loop that iterates over each item in pokemonList
+for (let i=0; i< pokemonList.length; i++){
+  document.write(pokemonList[i].name);
+}
