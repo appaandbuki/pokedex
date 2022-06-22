eef79ba0a7adf085051190cfde393e5dd2dfa69e
appaandbuki <104958634+appaandbuki@users.noreply.github.com>
2022-06-21 18:36:15 -0700

    created "for" loop for pokemon array

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,31 @@
 let pokemonList= [
-  { name: 'Bulbasaur', height: .7, types: 'grass', 'poison' },
-  { name: 'Charmander', height: .6, types: 'fire' },
-  { name: 'Squirtle', height: .5, types: 'water' },
-  { name: 'Pikachu', height: .4, types: 'electric' },
+  {
+    name: 'Bulbasaur',
+    height: .7,
+    types: ['grass', 'poison']
+  },
+  {
+    name: 'Charmander',
+    types: ['fire']
+  },
+  {
+    name: 'Squirtle',
+    height: .5,
+    types: ['water']
+  },
+  {
+    name: 'Pikachu',
+    height: .4,
+    types:[ 'electric'] },
 ];
 //create a for loop that iterates over each item in pokemonList
-for (let i=0; i< pokemonList.length; i++){
-  document.write(pokemonList[i].name);
+for (let i=0; i<pokemonList.length; i++){
+  if (pokemonList[i].height > .5)
+  {
+    document.write(pokemonList[i].name + " (" + "height " + pokemonList[i].height + ") " + "- Wow he is chonky <br />");
+  }
+  else
+  {
+    document.write(pokemonList[i].name + " (" + "height " + pokemonList[i].height + ") <br />");
+  }
 }
